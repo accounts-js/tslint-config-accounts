9aa0c5300a1c00245ec445403c2891817f942693
David Yahalomi <davidyaha@gmail.com>
2017-09-16 22:02:07 +0300

    Added submodule exception for lodash

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ module.exports = {
   rules: {
     "interface-name": [false],
     "object-literal-sort-keys": false,
-    "ordered-imports": [false]
+    "ordered-imports": [false],
+    "no-submodule-imports": [true, "lodash"]
   },
 };
